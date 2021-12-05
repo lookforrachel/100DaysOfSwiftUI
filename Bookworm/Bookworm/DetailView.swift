f052b9d08213d9ad0f954e15bf950812e7e98ebf
Lookforrachel <lookfor.rachel@gmail.com>
2021-12-04 21:05:00 -0500

    Day 55: App Complete

---
+++ Bookworm/Bookworm/DetailView.swift
@@ -0,0 +1,75 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Rachel Yee on 12/4/21.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var showingAlert = false
+    
+    let book: Book
+    
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre ?? "Fantasy")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre?.uppercased() ?? "FANTASY")
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                    .offset(x:-5, y:-5)
+            }
+            
+            Text(book.author ?? "Unknown Author")
+                .font(.title)
+                .foregroundColor(.secondary)
+            
+            Text(book.review ?? "No Review")
+                .padding()
+            
+            RatingView(rating: .constant(Int(book.rating)))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title ?? "Unknown Book")
+        .navigationBarTitleDisplayMode(.inline)
+        .alert("Delete Book", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive, action: deleteBook)
+            Button("Cancel", role: .cancel) {}
+        } message: {
+            Text("Are you sure?")
+        }
+        .toolbar {
+            Button {
+                showingAlert = true
+            } label: {
+                Label("Delete this book", systemImage: "trash")
+            }
+        }
+        
+    }
+    
+    func deleteBook() {
+        moc.delete(book)
+        
+//        try? moc.save()
+        dismiss()
+    }
+}
+
+//struct DetailView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DetailView()
+//    }
+//}
