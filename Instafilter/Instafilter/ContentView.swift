f01fb5859ff26427eb54243666d90657b5e817e9
Lookforrachel <lookfor.rachel@gmail.com>
2021-12-12 18:04:49 -0500

    Day 62: Responding to state changes using onChange()

---
+++ Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Rachel Yee on 12/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var blurAmount = 0.0
+    
+    var body: some View {
+        VStack {
+            Text("Hello World")
+                .blur(radius: blurAmount)
+            
+            Slider(value: $blurAmount, in: 0...20)
+
+            
+            Button("Random Blur") {
+                blurAmount = Double.random(in: 0...20)
+            }
+        }
+        .onChange(of: blurAmount) { newValue in
+            print("New value is \(newValue)")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
