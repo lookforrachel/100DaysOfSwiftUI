6f8f3c01d0d8e95039fa4fae0a45582d1ed4c10c
Lookforrachel <lookfor.rachel@gmail.com>
2021-12-19 18:19:30 -0500

    Day 68: Writing data to the documents directory

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -7,34 +7,30 @@
 
 import SwiftUI
 
-struct User: Identifiable, Comparable {
-
-    let id = UUID()
-    let firstName: String
-    let lastName: String
-
-    static func < (lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
-    }
-}
-
 struct ContentView: View {
-//    let values = [2,9,6,4,7].sorted()
-    
-    let users = [
-        User(firstName: "Harry", lastName: "Potter"),
-        User(firstName: "Ron", lastName: "Weasley"),
-        User(firstName: "Hermione", lastName: "Granger")
-    ].sorted()
+
     
     var body: some View {
-//        List(values, id: \.self){
-//            Text(String($0))
-//        }
-        
-        List(users){ user in
-            Text("\(user.firstName) \(user.lastName)")
-        }
+        Text("Hello World")
+            .onTapGesture {
+                let str = "Test message"
+                let url = getDocumentDirectory().appendingPathComponent("message.txt")
+                do {
+                    try str.write(to:url, atomically: true, encoding: .utf8)
+                    
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+    
+
+    }
+    
+    func getDocumentDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
 }
 
