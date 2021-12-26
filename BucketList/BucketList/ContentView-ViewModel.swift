61909df61c0c330f19489f18eeee2a7b2600b16b
Lookforrachel <lookfor.rachel@gmail.com>
2021-12-25 21:58:48 -0500

    Day 72: Locking our UI behind Face ID

--- BucketList/BucketList/ContentView-ViewModel.swift
+++ BucketList/BucketList/ContentView-ViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import LocalAuthentication
 import MapKit
 
 extension ContentView {
@@ -13,6 +14,7 @@ extension ContentView {
         @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 1, longitude: 104), span: MKCoordinateSpan(latitudeDelta: 0, longitudeDelta: 1))
         @Published private(set) var locations: [Location]
         @Published var selectedPlace: Location?
+        @Published var isUnlocked = false
         
         let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
         
@@ -47,6 +49,27 @@ extension ContentView {
             if let index = locations.firstIndex(of: selectedPlace) {
                 locations[index] = location
                 save()
+            }
+        }
+        
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate yourself to unlock your places"
+            
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {success, authenticationError in
+                    if success {
+                        Task { @MainActor in
+                                self.isUnlocked = true
+                        }
+                    } else {
+                        //error
+                    }
+                }
+            } else {
+                
             }
         }
     }
