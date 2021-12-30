82cb00159ebde0fb6618a47bf3f8a8df9074df24
Lookforrachel <lookfor.rachel@gmail.com>
2021-12-29 22:56:13 -0500

    Day 74: Identifying views with useful labels

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Rachel Yee on 12/29/21.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
