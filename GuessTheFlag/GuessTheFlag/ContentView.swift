d9d1c9f543b4c784df92d93879dc80ff0ba1cdf5
Lookforrachel <lookfor.rachel@gmail.com>
2021-12-30 17:29:59 -0500

    Day 75: Fixing accessibility problems for Guess The Flag game

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -54,6 +54,20 @@ struct ContentView: View {
 //    @State private var opacityAmount = 100.0
 //    @State private var scaleAmount = 1.0
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     
     var body: some View {
         ZStack{
@@ -76,7 +90,6 @@ struct ContentView: View {
 
                             Text(countries[correctAnswer])
                             .font(.largeTitle.weight(.semibold))
-
                         }
                     
                     ForEach(0..<3, id: \.self) { number in
@@ -92,6 +105,8 @@ struct ContentView: View {
                             }
                         } label: {
                             FlagImage(text: countries[number])
+                                .accessibilityLabel(labels[countries[number], default: "Unknown"])
+
                         }
 //                        .rotation3DEffect(.degrees(self.selection == number ? self.animationAmount : 0), axis: (x: 0, y: 1, z: 0))
 //                        .opacity(self.selection == number ? 1 : self.opacityAmount)
@@ -103,7 +118,7 @@ struct ContentView: View {
                 .padding(.vertical, 20)
                 .background(.regularMaterial)
                 .clipShape(RoundedRectangle(cornerRadius: 20))
-
+                
                 Spacer()
                 Spacer()
                 
