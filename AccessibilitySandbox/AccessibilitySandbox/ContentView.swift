f2e2f5e775e703b21a4a5bf22042ac8302d78755
Lookforrachel <lookfor.rachel@gmail.com>
2021-12-29 23:21:11 -0500

    Day 74: Reading the value of controls

--- AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -8,20 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    @State private var value = 10
     
     var body: some View {
-//        Image(decorative: "character")
-//            .accessibilityHidden(true)
-        
         VStack {
-            Text("Your score is")
-            Text("1000")
-                .font(.title)
+            Text("Value: \(value)")
+            Button("Increment"){
+                value += 1
+            }
+            Button("Decrement"){
+                value -= 1
+            }
         }
-//        .accessibilityElement(children: .combine)
-        .accessibilityElement(children: .ignore)
-        .accessibilityLabel("Your score is 1000")
+        .accessibilityElement()
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment: value += 1
+            case .decrement: value -= 1
+            default: print("Not handled")
+            }
+        }
     }
 }
 
