84f74693ee2df656999c061dae82508e28923ecc
Lookforrachel <lookfor.rachel@gmail.com>
2022-01-22 20:38:32 -0500

    Day 79: Reading custom values from the environment with @EnvironmentObject

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Rachel Yee on 1/22/22.
+//
+
+import SwiftUI
+
+@MainActor class User: ObservableObject {
+    @Published var name = "Taylor Swift"
+}
+
+struct EditView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        TextField("Name", text: $user.name)
+    }
+}
+
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        Text(user.name)
+    }
+}
+
+struct ContentView: View {
+    @StateObject var user = User()
+    
+    var body: some View {
+        VStack {
+            EditView()
+            DisplayView()
+        }
+        .environmentObject(user)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Rachel Yee on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
