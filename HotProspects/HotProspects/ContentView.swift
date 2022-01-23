290e59e9a8712e5168488324442661f8e47e701e
Lookforrachel <lookfor.rachel@gmail.com>
2022-01-22 21:25:51 -0500

    Day 79: Creating tabs with TabView and tabItem(). Not sure if onTapGesture is doing anything.

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -7,35 +7,26 @@
 
 import SwiftUI
 
-@MainActor class User: ObservableObject {
-    @Published var name = "Taylor Swift"
-}
-
-struct EditView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        TextField("Name", text: $user.name)
-    }
-}
-
-struct DisplayView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        Text(user.name)
-    }
-}
-
 struct ContentView: View {
-    @StateObject var user = User()
-    
+    static let tag = "Two"
+    @State private var selectedTab = "One"
     var body: some View {
         VStack {
-            EditView()
-            DisplayView()
+            TabView(selection: $selectedTab) {
+                Text("Tab 1")
+                    .onTapGesture {
+                        selectedTab = ContentView.tag
+                    }
+                    .tabItem {
+                        Label("One", systemImage: "star")
+                    }
+                Text("Tab 2")
+                    .tabItem {
+                        Label("Two", systemImage: "circle")
+                    }
+                    .tag(ContentView.tag)
+            }
         }
-        .environmentObject(user)
     }
 }
 
