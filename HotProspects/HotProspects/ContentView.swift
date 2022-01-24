35cf6a425f3c572639fb4d4c1a24fd2cecdc7836
Lookforrachel <lookfor.rachel@gmail.com>
2022-01-23 22:00:05 -0500

    Day 80: Controlling image interpolation in SwiftUI

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,38 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var output = ""
     
     var body: some View {
-        Text("Value is \(output)")
-            .task {
-                await fetchReadings()
-            }
+        Image("example")
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .frame(maxHeight: .infinity)
+            .background(.black)
+            .ignoresSafeArea()
     }
     
-    func fetchReadings() async {
-        let fetchTask = Task { () -> String in
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings"
-        }
-        let result = await fetchTask.result
-        
-//        do {
-//            output = try result.get()
-//        } catch {
-//            print("Download error")
-//        }
-        
-        switch result {
-        case .success(let str):
-            output = str
-        
-        case .failure(let error):
-            output = "Download error: \(error.localizedDescription))"
-        }
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
