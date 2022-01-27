251f87a327be35c5264d2755ca82c205ceb649e9
Lookforrachel <lookfor.rachel@gmail.com>
2022-01-26 21:41:19 -0500

    Day 83: Generating and scaling up a QR code, scanning QR codes with SwiftUI, Adding options with swipe actions

--- HotProspects/HotProspects/ProspectsView.swift
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -5,6 +5,7 @@
 //  Created by Rachel Yee on 1/25/22.
 //
 
+import CodeScanner
 import SwiftUI
 
 struct ProspectsView: View {
@@ -13,6 +14,7 @@ struct ProspectsView: View {
     }
     
     @EnvironmentObject var prospects: Prospects
+    @State private var isShowingScanner = false
     
     let filter: FilterType
     
@@ -25,20 +27,36 @@ struct ProspectsView: View {
                             .font(.headline)
                         Text(prospect.emailAddress)
                             .foregroundColor(.secondary)
-                        
+                    }
+                    .swipeActions {
+                        if prospect.isContacted {
+                            Button {
+                                prospects.toggle(prospect)
+                            } label: {
+                                Label("Mark uncontacted", systemImage: "person.crop.circle.badge.xmark")
+                            }
+                            .tint(.blue)
+                        } else {
+                            Button {
+                                prospects.toggle(prospect)
+                            } label: {
+                                Label("Mark contacted", systemImage: "person.crop.circle.fill.badge.checkmark")
+                            }
+                            .tint(.green)
+                        }
                     }
                 }
             }
             .navigationTitle(title)
             .toolbar {
                 Button {
-                    let prospect = Prospect()
-                    prospect.name = "Paul Hudson"
-                    prospect.emailAddress = "paul@hackingwithswift.com"
-                    prospects.people.append(prospect)
+                    isShowingScanner = true
                 } label: {
                     Label("Scan", systemImage: "qrcode.viewfinder")
                 }
+            }
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: handleScan)
             }
         }
     }
@@ -53,6 +71,7 @@ struct ProspectsView: View {
             return "Uncontacted people"
         }
     }
+    
     var filteredProspects: [Prospect] {
         switch filter {
         case .none:
@@ -61,6 +80,24 @@ struct ProspectsView: View {
             return prospects.people.filter { $0.isContacted}
         case .uncontacted:
             return prospects.people.filter { !$0.isContacted}
+        }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+            prospects.people.append(person)
+
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
         }
     }
 }
