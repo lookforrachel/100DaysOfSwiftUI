d1e4b1ab37d3b600f8df7caa922d250ebbbeeda3
Lookforrachel <lookfor.rachel@gmail.com>
2022-01-31 20:58:20 -0500

    Day 87: Triggering events repeatedly using a timer

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -9,34 +9,19 @@ import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
  
     var body: some View {
-        VStack {
-            Text("Hello")
-            Spacer().frame(height: 100)
-            Text("World")
-        }
-        .contentShape(Rectangle())
-        .onTapGesture {
-            print("VStack tapped")
-        }
-//        ZStack {
-//            Rectangle()
-//                .fill(.blue)
-//                .frame(width: 300, height: 300)
-//                .onTapGesture {
-//                    print("Rectangle tapped")
-//                }
-//
-//            Circle()
-//                .fill(.red)
-//                .frame(width: 300, height: 300)
-//                .contentShape(Rectangle())
-//                .onTapGesture {
-//                    print("Circle tapped")
-//                }
-////                .allowsHitTesting(false)
-//        }
+        Text("Hello")
+            .onReceive(timer) { time in
+                if counter == 5 {
+                    timer.upstream.connect().cancel()
+                } else {
+                print("The time is now \(time)")
+                }
+                counter += 1
+            }
     }
 }
 
