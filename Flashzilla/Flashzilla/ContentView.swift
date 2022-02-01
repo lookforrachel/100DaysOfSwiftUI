879eea86878ab6f3d9634394a98b94e46f524434
Lookforrachel <lookfor.rachel@gmail.com>
2022-01-31 21:05:05 -0500

    Day 87: SwiftUI app moves to the background

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -9,18 +9,20 @@ import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
-    @State private var counter = 0
- 
+    @Environment(\.scenePhase) var scenePhase
+    
     var body: some View {
         Text("Hello")
-            .onReceive(timer) { time in
-                if counter == 5 {
-                    timer.upstream.connect().cancel()
-                } else {
-                print("The time is now \(time)")
+            .padding()
+            .onChange(of: scenePhase) { newPhase in
+                if newPhase == .active {
+                    print("Active")
+                } else if newPhase == .inactive {
+                    print("Inactive")
+                } else if newPhase == .background {
+                    print("Background")
                 }
-                counter += 1
+                
             }
     }
 }
