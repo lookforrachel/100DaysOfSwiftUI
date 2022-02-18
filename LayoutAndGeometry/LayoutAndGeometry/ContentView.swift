ad6e4f6cfc85f4ba58a6e9fc85fc71f5984223f5
Lookforrachel <lookfor.rachel@gmail.com>
2022-02-17 22:15:58 -0500

    Day 92: How to create a custom alignment guide

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,43 +7,44 @@
 
 import SwiftUI
 
+extension VerticalAlignment {
+    enum MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[.top]
+        }
+    }
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
 struct ContentView: View {
     var body: some View {
-        
-        VStack(alignment: .leading) {
-            ForEach(0..<10) { position in
-                Text("Number \(position)")
-                    .alignmentGuide(.leading) { _ in
-                        Double(position) * -10
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@twostraws")
+                    .alignmentGuide(.midAccountAndName) { d in
+                        d[VerticalAlignment.center]
                     }
+                Image("Chickens")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+                Text("Full name:")
+                Text("Full name:")
+                Text("Full name:")
+                Text("Full name:")
+
+                
+            }
+            VStack{
+                Text("Full name:")
+                Text("Full name:")
+                Text("Full name:")
+                Text("Full name:")
+                Text("PAUL HUDSON")
+                    .alignmentGuide(.midAccountAndName) { d in
+                        d[VerticalAlignment.center]}
+                    .font(.largeTitle)
             }
         }
-        
-//        VStack(alignment: .leading) {
-//            Text("Hello, world!")
-////                .offset(x: -50)
-//                .alignmentGuide(.leading) { d in
-//                    d[.trailing]
-//                }
-//
-//            Text("This is a longer line of text")
-//        }
-                .background(.red)
-                .frame(width: 400, height: 400)
-                .background(.blue)
-//        HStack(alignment: .lastTextBaseline) {
-//            Text("Live")
-//                .font(.caption)
-//            Text("long")
-//            Text("and")
-//                .font(.title)
-//            Text("prosper")
-//                .font(.largeTitle)
-//        }
-        
-//        Text("Live long and prosper")
-//            .frame(width: 300, height: 300, alignment: .topLeading)
-//            .offset(x: 50, y: 50)
     }
 }
 
