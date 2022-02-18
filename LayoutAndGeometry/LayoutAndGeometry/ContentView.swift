b708163d85eae7782326d957e89ff538a1cc9aa8
Lookforrachel <lookfor.rachel@gmail.com>
2022-02-17 22:02:42 -0500

    Day 92: Alignment and alignment guides

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,11 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Color.red
         
-//        Text("Hello, World!")
-//            .padding(20)
-//            .background(.red)
+        VStack(alignment: .leading) {
+            ForEach(0..<10) { position in
+                Text("Number \(position)")
+                    .alignmentGuide(.leading) { _ in
+                        Double(position) * -10
+                    }
+            }
+        }
+        
+//        VStack(alignment: .leading) {
+//            Text("Hello, world!")
+////                .offset(x: -50)
+//                .alignmentGuide(.leading) { d in
+//                    d[.trailing]
+//                }
+//
+//            Text("This is a longer line of text")
+//        }
+                .background(.red)
+                .frame(width: 400, height: 400)
+                .background(.blue)
+//        HStack(alignment: .lastTextBaseline) {
+//            Text("Live")
+//                .font(.caption)
+//            Text("long")
+//            Text("and")
+//                .font(.title)
+//            Text("prosper")
+//                .font(.largeTitle)
+//        }
+        
+//        Text("Live long and prosper")
+//            .frame(width: 300, height: 300, alignment: .topLeading)
+//            .offset(x: 50, y: 50)
     }
 }
 
