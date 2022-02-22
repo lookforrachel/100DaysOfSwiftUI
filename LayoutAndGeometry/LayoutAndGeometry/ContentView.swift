c0ac5d69f7b021854b953057e304eb35bb16e345
Lookforrachel <lookfor.rachel@gmail.com>
2022-02-21 20:35:22 -0500

    Day 94: Challenges complete. Need to figure out how to use max min () to handle scaling so that views don't go smaller than half their size & with hue so that hue values don't go beyond 1.0.

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -11,42 +11,31 @@ struct ContentView: View {
     let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
 
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 0) {
-                ForEach(1..<20) { num in
+        // reads all available screen space
+        GeometryReader { fullView in
+            ScrollView {
+                ForEach(0..<50) { index in
+                    
+                    // reads current screen space for internal height row
                     GeometryReader { geo in
-                        Text("Number \(num)")
-                            .font(.largeTitle)
-                            .padding()
-                            .background(.red)
-                        // proxy's frame in the global namespace, give me the left edge, the-x value from that. Give me a fraction of tt - / 8.
-                            .rotation3DEffect(.degrees(-geo.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
+                        Text("Row #\(index), geo: \(geo.frame(in: .global).minY / 200)")
+//                            .font(.title)
+//                            .frame(maxWidth: .infinity)
+                            .frame(width: abs(geo.frame(in: .global).minY))
+//                            .background(colors[index % 7])
+                            .background(Color(hue: geo.frame(in: .global).minY / 600, saturation: 1.0, brightness: 0.5)
+)
+                            .opacity(abs(geo.frame(in: .global).minY / 200))
+
+                        // minY - half available size / 5
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            
+
                     }
-                    .frame(width: 200, height: 200)
+                    .frame(height: 40)
                 }
             }
         }
-        
-//        // reads all available screen space
-//        GeometryReader { fullView in
-//            ScrollView {
-//                ForEach(0..<50) { index in
-//
-//                    // reads current screen space for internal height row
-//                    GeometryReader { geo in
-//                        Text("Row #\(index)")
-//                            .font(.title)
-//                            .frame(maxWidth: .infinity)
-//                            .background(colors[index % 7])
-//
-//                        // minY - half available size / 5
-//                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
-//
-//                    }
-//                    .frame(height: 40)
-//                }
-//            }
-//        }
     }
 }
 
