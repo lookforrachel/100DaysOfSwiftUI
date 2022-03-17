e1e9c2f6848a4bdf56bdcdcb2b88d125a24ae681
Lookforrachel <lookfor.rachel@gmail.com>
2022-03-16 21:36:08 -0400

    Day 97: Building a primary list of items

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -8,23 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var searchText = ""
-    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
     
     var body: some View {
         NavigationView {
-            List(filteredNames, id:\.self) { name in
-                Text(name)
-            }
-                .searchable(text: $searchText, prompt: "Look for something")
-                .navigationTitle("Searching")
-        }
-    }
-    var filteredNames: [String] {
-        if searchText.isEmpty {
-            return allNames
-        } else {
-            return allNames.filter {$0.localizedCaseInsensitiveContains(searchText)}
+                List(resorts) { resort in
+                    NavigationLink {
+                        Text(resort.name)
+                    } label: {
+                        Image(resort.country)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 40, height: 25)
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(.black, lineWidth: 1)
+                            )
+                        VStack(alignment: .leading) {
+                            Text(resort.name)
+                                .font(.headline)
+                            Text("\(resort.runs) runs")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
+                .navigationTitle("Resorts")
         }
     }
     
---
+++ SnowSeeker/SnowSeeker/Resort.swift
@@ -0,0 +1,27 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Rachel Yee on 3/16/22.
+//
+
+import Foundation
+
+struct Resort: Codable, Identifiable {
+    let id: String
+    let name: String
+    let country: String
+    let description: String
+    let imageCredit: String
+    let price: Int
+    let size: Int
+    let snowDepth: Int
+    let elevation: Int
+    let runs: Int
+    let facilities: [String]
+    
+    static let allResorts: [Resort] = Bundle.main.decode("resorts.json")
+    static let example = allResorts[0]
+    
+//    static let example = (Bundle.main.decode("resorts.json") as [Resort])[0]
+}
