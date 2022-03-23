aa11c13bc53d21265d8fab585d6ecc3a62910079
Lookforrachel <lookfor.rachel@gmail.com>
2022-03-22 21:40:27 -0400

    Day 97: Making NavigationView work in landscape

---
+++ SnowSeeker/SnowSeeker/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  SnowSeeker
+//
+//  Created by Rachel Yee on 3/22/22.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
