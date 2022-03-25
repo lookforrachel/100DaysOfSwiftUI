cf26d14af2395b71151bbd510f07ca6d685b2d12
Lookforrachel <lookfor.rachel@gmail.com>
2022-03-24 20:48:56 -0400

    Day 98: Letting the user mark favorites

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -19,6 +19,8 @@ extension View {
 
 struct ContentView: View {
     let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    
+    @StateObject var favorites = Favorites()
     @State private var searchText = ""
     
     var body: some View {
@@ -27,20 +29,28 @@ struct ContentView: View {
                     NavigationLink {
                         ResortView(resort: resort)
                     } label: {
-                        Image(resort.country)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 40, height: 25)
-                            .clipShape(RoundedRectangle(cornerRadius: 5))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 5)
-                                    .stroke(.black, lineWidth: 1)
-                            )
-                        VStack(alignment: .leading) {
-                            Text(resort.name)
-                                .font(.headline)
-                            Text("\(resort.runs) runs")
-                                .foregroundColor(.secondary)
+                        HStack {
+                            Image(resort.country)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 25)
+                                .clipShape(RoundedRectangle(cornerRadius: 5))
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .stroke(.black, lineWidth: 1)
+                                )
+                            VStack(alignment: .leading) {
+                                Text(resort.name)
+                                    .font(.headline)
+                                Text("\(resort.runs) runs")
+                                    .foregroundColor(.secondary)
+                            }
+                            if favorites.contains(resort) {
+                                Spacer()
+                                Image(systemName: "heart.fill")
+                                    .accessibilityLabel("This is a favorite resort")
+                                    .foregroundColor(.red)
+                            }
                         }
                     }
                 }
@@ -49,6 +59,7 @@ struct ContentView: View {
             
             WelcomeView()
         }
+        .environmentObject(favorites)
         .phoneOnlyNavigationView()
     }
     var filteredResorts: [Resort] {
--- SnowSeeker/SnowSeeker/ResortView.swift
+++ SnowSeeker/SnowSeeker/ResortView.swift
@@ -13,6 +13,8 @@ struct ResortView: View {
     @Environment(\.horizontalSizeClass) var sizeClass
     @Environment(\.dynamicTypeSize) var typeSize
     
+    @EnvironmentObject var favorites: Favorites
+    
     @State private var selectedFacility: Facility?
     @State private var showingFacility = false
     
@@ -54,10 +56,18 @@ struct ResortView: View {
                             }
                         }
                     }
-                    
-                    Text(resort.facilities, format: .list(type: .and))
-                        .padding(.horizontal)
+                    Button(favorites.contains(resort) ? "Remove frorm Favorites" : "Add to Favorites") {
+                        if favorites.contains(resort) {
+                            favorites.remove(resort)
+                        } else {
+                            favorites.add(resort)
+                        }
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .padding()
                 }
+                .padding(.horizontal)
+
             }
             .navigationTitle("\(resort.name), \(resort.country)")
             .navigationBarTitleDisplayMode(.inline)
@@ -70,6 +80,9 @@ struct ResortView: View {
 
 struct ResortView_Previews: PreviewProvider {
     static var previews: some View {
-        ResortView(resort: Resort.example)
+        NavigationView {
+            ResortView(resort: Resort.example)
+        }
+        .environmentObject(Favorites())
     }
 }
